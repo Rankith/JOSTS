392fd7363de17d849783875c1d95b65e205743ab
Julian Ridley <rankith117@hotmail.com>
2019-10-09 14:12:53 -0700

    Skills Screen and BS4

--- JOSTS/app/models.py
+++ JOSTS/app/models.py
@@ -17,7 +17,7 @@ class Skill(models.Model):
     event = models.CharField(max_length=2)
     str_grp = models.IntegerField(default=0)
     code_order = models.IntegerField(default=0)
-    id_number = models.IntegerField(default=0)
+    id_number = models.CharField(max_length=20)
     letter_value = models.CharField(max_length=1,default='A')
     value = models.DecimalField(max_digits=3,default=0.1,decimal_places=1)
     range = models.CharField(max_length=25,default='',blank=True)
@@ -27,7 +27,7 @@ class Skill(models.Model):
     def __str__(self):
         return self.event + " " + str(self.id_number)
     def image_url(self):
-        return self.event + str(self.id_number)
+        return self.event + self.id_number.replace(".","")
 
 class EnglishSkill(models.Model):
     skill = models.ForeignKey(Skill, on_delete=models.SET_NULL,null=True)
