a3b4c94ded2df1f2ad8cdf3d4b69f9948b2eeda0
Julian <Julian@MAIN-PC>
2019-10-15 20:02:47 -0700

    Import and reset

--- JOSTS/app/admin.py
+++ JOSTS/app/admin.py
@@ -1,12 +1,20 @@
 from django.contrib import admin
-from app.models import Element, ElementText, Video, UserNote
+from app.models import Element, ElementText, Video, UserNote 
+from import_export import resources
+from import_export.admin import ImportExportModelAdmin
 
-class ElementAdmin(admin.ModelAdmin):
-    list_display = ('id_number','str_grp','code_order','event','letter_value','value')
+class ElementAdmin(ImportExportModelAdmin):
+    list_display = ('id_number','str_grp','code_order','event','letter_value','value','id')
 class UserNoteAdmin(admin.ModelAdmin):
     list_display = ('user','element','note')
+class ElementTextAdmin(ImportExportModelAdmin):
+    list_display = ('element','language','text','short_text','named','additional_info')
+
+class ElementResource(resources.ModelResource):
+    class Meta:
+        model = Element
 
 admin.site.register(Element,ElementAdmin)
-admin.site.register(ElementText)
+admin.site.register(ElementText,ElementTextAdmin)
 admin.site.register(Video)
 admin.site.register(UserNote,UserNoteAdmin)
