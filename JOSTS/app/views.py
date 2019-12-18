52d68c93e117f2486f6c4b19680c696c3a6ddac2
Julian Ridley <rankith117@hotmail.com>
2019-12-18 12:55:55 -0800

    shorthand quiz swap and about

--- JOSTS/app/views.py
+++ JOSTS/app/views.py
@@ -48,16 +48,7 @@ def contact(request):
 
 def about(request):
     """Renders the about page."""
-    assert isinstance(request, HttpRequest)
-    return render(
-        request,
-        'app/about.html',
-        {
-            'title':'About',
-            'message':'Your application description page.',
-            'year':datetime.now().year,
-        }
-    )
+    return render(request,'app/about.html')
 
 def log_activity(user,action_type,action_detail,action_item):
     AL = ActivityLog(actor=user,action_type=action_type,action_detail=action_detail,action_item=action_item)
