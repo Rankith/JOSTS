056f2c07ed1529da869e2544752b466e28d98ba5
Julian <Julian@MAIN-PC>
2020-03-20 10:56:21 -0700

    settings for showing new videos.  Layout fixes

---
+++ JOSTS/app/context_processors.py
@@ -0,0 +1,7 @@
+from django.conf import settings # import the settings file
+
+def video_settings(request):
+    # return the value you want as a dictionnary. you may add multiple values in there.
+    return {'SHOW_COMPETITION_VIDEOS': settings.SHOW_COMPETITION_VIDEOS,
+            'SHOW_JUDGE_INSTRUCTIONS': settings.SHOW_JUDGE_INSTRUCTIONS,
+            'SHOW_TC_EXAMPLES': settings.SHOW_TC_EXAMPLES}
