0b2cee30b204615a722fc7f36613626fc107b8fe
Julian <Julian@MAIN-PC>
2020-05-08 11:04:55 -0700

    mark approved

--- JOSTS/app/views.py
+++ JOSTS/app/views.py
@@ -348,6 +348,8 @@ def element_search(request):
     ranges = Element.objects.filter(disc=request.session.get('disc',1)).order_by('range').values('range').exclude(range='').annotate(int_order=Cast('range',IntegerField())).order_by('int_order').distinct()
     groupDict = {}
     valueDict = {}
+    Video.objects.filter(id__in=TCExample.objects.all().values_list('video__id')).update(approved_liason=True,approved_final=True,approved_sts=True)
+
     #Video.objects.update(file=Replace('file',Value('.mov'),Value('.mp4')))
     #RuleLink.objects.update(type=Left('type',1))
     #vvals = Element.objects.filter(letter_value='A').update(down_value_letter='A')
