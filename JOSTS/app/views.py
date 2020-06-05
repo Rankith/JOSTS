281ed830b671a815847dd00ee08fd3c9b8d790f8
Julian <Julian@MAIN-PC>
2020-06-05 14:00:07 -0700

    minor fixes

--- JOSTS/app/views.py
+++ JOSTS/app/views.py
@@ -385,10 +385,10 @@ def element_search(request):
 
         events=request.session.get('disc_events','V,UB,BB,FX').split(",")
         for group in groups:
-            groupEvents = "search-" + " search-".join(str(events['event']) for events in Element.objects.filter(str_grp = group['str_grp']).order_by('event').values('event').distinct())
+            groupEvents = "search-" + " search-".join(str(events['event']) for events in Element.objects.filter(str_grp = group['str_grp'],disc=request.session.get('disc',1)).order_by('event').values('event').distinct())
             groupDict[group['str_grp']] = groupEvents
         for value in vals:
-            valueEvents = "search-" + " search-".join(str(events['event']) for events in Element.objects.filter(letter_value = value['letter_value']).order_by('event').values('event').distinct())
+            valueEvents = "search-" + " search-".join(str(events['event']) for events in Element.objects.filter(letter_value = value['letter_value'],disc=request.session.get('disc',1)).order_by('event').values('event').distinct())
             valueDict[value['letter_value']] = valueEvents
         context = {
             'vals':vals,
